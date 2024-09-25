31639da5750fddb0dfaaed06c8dddd1904d02d46
DeDxYk594 <zhugeo@mail.ru>
2024-09-26 02:27:53 +0300

    Отформатирован index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { RenderJSX } from "./jsx_core/renderer.js";
 const someElements = (
   <>
-    <a href="https://example.com"><div>dfdf</div></a>
+    <a href="https://example.com">
+      <div>dfdf</div>
+    </a>
     <h1 href="https://example.com">adsadsf</h1>
   </>
 );
-
-console.log("someElements",someElements)
 
 const root = document.getElementById("root");
 if (root != null) {
