f6e2972ffa01750ba4fc7b9edf168ec902ca2590
DeDxYk594 <zhugeo@mail.ru>
2024-09-29 01:00:52 +0300

    Сделаны константами HTTP-статусы

--- server.ts
+++ server.ts
@@ -1,68 +1,73 @@
-import express from "express";
-import fs from "fs";
-import path from "path";
-import { compileTs } from "./dev_server/ts_compile";
-import { exit } from "process";
-import nocache from "nocache";
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { compileTs } from './dev_server/ts_compile';
+import nocache from 'nocache';
 
 const app = express();
 app.use(nocache());
 const port = 3000;
 
+const HTTP_STATUS_NOT_FOUND = 404;
+const HTTP_STATUS_METHOD_NOT_ALLOWED = 405;
+const HTTP_STATUS_NOT_ACCEPTABLE = 406;
+const HTTP_STATUS_INTERNAL_ERROR = 500;
+
 const mimeTypeMap: any = {
-  css: "text/css",
-  html: "text/html",
-  js: "text/javascript",
-  woff: "font/woff",
-  woff2: "font/woff2",
-  ttf: "font/ttf",
-  svg: "image/svg+xml",
+  css: 'text/css',
+  html: 'text/html',
+  js: 'text/javascript',
+  woff: 'font/woff',
+  woff2: 'font/woff2',
+  ttf: 'font/ttf',
+  svg: 'image/svg+xml',
 };
 
-console.log("Заводим сервак...");
+console.log('Заводим сервак...');
 
 const devServer = (req: express.Request, res: express.Response) => {
   let name = req.url;
-  if (name == "" || name == "/") name = "index.html";
-  if (name.endsWith(".ts") || name.endsWith(".tsx")) {
-    res.status(400);
-    res.send();
-    return;
-  }
+  if (name === '' || name === '/') name = 'index.html';
   const method = req.method;
   console.log(`${method} ${name}`);
-  if (method !== "GET") {
-    res.status(400).send("Only GET requests are allowed for frontend");
+  if (method !== 'GET') {
+    res.status(HTTP_STATUS_METHOD_NOT_ALLOWED).send();
+    return;
+  }
+  if (name.endsWith('.ts') || name.endsWith('.tsx')) {
+    // После компиляции все ts и tsx файлы становятся .js
+    res.status(HTTP_STATUS_NOT_ACCEPTABLE);
+    res.send();
     return;
   }
 
   let pathToFile: string;
-  const pathToBuild = path.join("build", name);
-  if (name.endsWith(".js")) pathToFile = pathToBuild;
-  else pathToFile = path.join("src", name);
+  const pathToBuild = path.join('build', name);
+  if (name.endsWith('.js')) pathToFile = pathToBuild;
+  else pathToFile = path.join('src', name);
 
   console.log(`Read file: ${pathToFile}`);
   if (!fs.existsSync(pathToFile)) {
-    res.status(404).send();
+    res.status(HTTP_STATUS_NOT_FOUND).send();
     return;
   }
   if (fs.statSync(pathToFile).isDirectory()) {
-    if (fs.existsSync(path.join(pathToBuild, "/index.js"))) {
-      res.redirect(req.url + "/index.js");
+    if (fs.existsSync(path.join(pathToBuild, '/index.js'))) {
+      res.redirect(req.url + '/index.js');
       return;
     }
-    res.status(400).send();
+    res.status(HTTP_STATUS_NOT_FOUND).send();
     return;
   }
   let fileContent: Buffer;
   try {
     fileContent = fs.readFileSync(pathToFile);
   } catch {
-    res.status(500);
+    res.status(HTTP_STATUS_INTERNAL_ERROR).send();
     return;
   }
 
-  const nameSplitted = name.split(".");
+  const nameSplitted = name.split('.');
   const nameExtension = nameSplitted[nameSplitted.length - 1];
   if (mimeTypeMap[nameExtension] !== undefined) {
     res.type(mimeTypeMap[nameExtension]);
@@ -73,12 +78,12 @@ const devServer = (req: express.Request, res: express.Response) => {
 
 const result = compileTs();
 if (result === false) {
-  app.use("/", (req, res) => {
-    res.type("text/html");
-    res.send(fs.readFileSync("dev_server/error_message.html", "utf8"));
+  app.use('/', (req, res) => {
+    res.type('text/html');
+    res.send(fs.readFileSync('dev_server/error_message.html', 'utf8'));
   });
 } else {
-  app.use("/", devServer);
+  app.use('/', devServer);
 }
 
 app.listen(3000, () => {
