598c0fe3132582151b4f2e9a731924e47e39994f
valentingit <v_stremin@mail.ru>
2024-10-21 00:34:44 +0300

    Начата разработка Роутера

---
+++ src/routes/routes.tsx
@@ -0,0 +1,12 @@
+export const routes = {
+    home: '/',
+    login: '/login',
+    register: '/register',
+    app: '/app',
+    board: '/app/board_id',
+    boardSettings: '/app/board_id/settings',
+    profile: '/app/profile',
+    kanbanBoard: '/app/board_id/kanban',
+    list: '/app/board_id/list',
+    search: '/app/board_id/search',
+}
