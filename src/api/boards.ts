82d451dc8410d1535daec511ba0d67e7e24c0acf
DeDxYk594 <zhugeo@mail.ru>
2024-10-20 15:29:48 +0300

    Обращения к API переделаны на async/await

--- src/api/boards.ts
+++ src/api/boards.ts
@@ -1,32 +1,34 @@
 import { Board } from '@/types/board';
-import { getApiUrl } from '@/api/apiHelper';
+import { getApiUrl, HTTP_STATUS_OK } from '@/api/apiHelper';
 
 /**
  * Получить все доступные пользователю доски
  * @returns Промис, который сходит на бэк и получит список доступных пользователю досок и вернёт их массивом
  */
-export const getBoards = (): Promise<Board[]> => {
-  return fetch(getApiUrl('/boards/my'), {
-    credentials: 'include',
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json().then((json: any[]) => {
-          return json.map((el): Board => {
-            return { id: el.id, title: el.name };
-          });
-        });
-      } else if (response.status === 404) {
-        alert('Board not found');
-      } else if (response.status === 401) {
-        alert('Get boards: User not authorized');
-      } else {
-        alert('Get boards: Unexpected error');
-      }
-    })
-    .then((data) => {
-      if (Array.isArray(data)) {
-        return data as Board[];
-      } else return [];
+export const getBoards = async (): Promise<Board[]> => {
+  try {
+    const response = await fetch(getApiUrl('/boards/my'), {
+      credentials: 'include',
     });
+
+    if (response.status === HTTP_STATUS_OK) {
+      const json = await response.json();
+      const boards: Board[] = json.map((el: any): Board => {
+        return { id: el.id, title: el.name };
+      });
+      return boards;
+    } else if (response.status === 404) {
+      alert('Board not found');
+    } else if (response.status === 401) {
+      alert('Get boards: User not authorized');
+    } else {
+      alert('Get boards: Unexpected error');
+    }
+
+    return [];
+  } catch (error) {
+    console.error('Error fetching boards:', error);
+    alert('An error occurred while fetching boards');
+    return [];
+  }
 };
--- src/stores/activeBoardStore.ts
+++ src/stores/activeBoardStore.ts
@@ -5,5 +5,5 @@ export const [useActiveBoardStore, setActiveBoardStore] =
   defineStore<ActiveBoard>('activeBoardStore', {
     columns: [],
     id: 0,
-    title: 'Моя самая любимая доска',
+    title: 'Загрузка...',
   });
