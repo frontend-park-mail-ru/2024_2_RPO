fa022cd19bee69086b68b023de4f9fc318ddf33b
Konstantin Safronov <kostya.safronov@gmail.com>
2024-10-29 02:00:39 +0300

    Добавлена проверка полей регистрации

--- src/containers/RegistrationDialog.tsx
+++ src/containers/RegistrationDialog.tsx
@@ -10,14 +10,24 @@ interface RegistrationDialogProps extends ComponentProps {
 }
 
 export const RegistrationDialog = (props: RegistrationDialogProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [data, setData] = useState({
     login: '',
     email: '',
     password: '',
     repeatPassword: '',
   });
-  console.log(data);
+
+  const validateEmail = (email: string) => {
+    if (email === '') return;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email) ? '' : 'Введите корректный email';
+  };
+
+  const validatePasswordMatch = (password: string, repeatPassword: string) => {
+    if (repeatPassword === '') return;
+    return password === repeatPassword ? '' : 'Пароли не совпадают';
+  };
+
   return (
     <ModalDialog
       key="modal_dialog"
@@ -26,9 +36,9 @@ export const RegistrationDialog = (props: RegistrationDialogProps) => {
       isOpened={true}
     >
       <div className="login-form__wrapper">
-        <div class="login-form">
+        <div className="login-form">
           <div style="display: flex; justify-content: end">
-            <label for="nickname">Логин:</label>
+            <label htmlFor="nickname">Логин:</label>
           </div>
           <Input
             key="nickname_input"
@@ -45,34 +55,47 @@ export const RegistrationDialog = (props: RegistrationDialogProps) => {
             })()}
           />
           <div style="display: flex; justify-content: end">
-            <label for="nickname">Email:</label>
+            <label htmlFor="email">Email:</label>
           </div>
           <Input
             key="email_input"
             onChanged={(newEmail) => {
               setData({ ...data, email: newEmail });
             }}
+            validationMessage={validateEmail(data.email)}
           />
           <div style="display: flex; justify-content: end">
-            <label for="nickname">Пароль:</label>
+            <label htmlFor="password">Пароль:</label>
           </div>
           <Input
             key="password_input"
             onChanged={(newPassword) => {
               setData({ ...data, password: newPassword });
             }}
+            validationMessage={(() => {
+              if (data.password === '') {
+                return;
+              }
+              if (data.password.length < 8) {
+                return 'Пароль должен быть не менее 8 символов';
+              }
+            })()}
           />
           <div style="display: flex; justify-content: end">
-            <label for="password">Повторите пароль:</label>
+            <label htmlFor="repeat_password">Повторите пароль:</label>
           </div>
           <Input
             key="repeat_password_input"
             onChanged={(newRepeatPassword) => {
               setData({ ...data, repeatPassword: newRepeatPassword });
             }}
+            validationMessage={validatePasswordMatch(
+              data.password,
+              data.repeatPassword
+            )}
           />
         </div>
-        <div class="login-form__button-container">
+        <div className="login-form__button-container">
           <Button
             key="submit_btn"
             variant="positive"
