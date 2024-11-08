ed213e147dbad8d62f16c0c031e2dfef5eb81e45
DeDxYk594 <zhugeo@mail.ru>
2024-11-08 14:29:11 +0300

    Добавлен Service Worker

--- src/App.tsx
+++ src/App.tsx
@@ -36,3 +36,19 @@ updateMe();
 setTimeout(() => {
   setRouterStore(getFlagRoutes(window.location.pathname));
 }, 0);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/static/sw.js').then(
+      function (registration) {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log('ServiceWorker registration failed: ', err);
+      }
+    );
+  });
+}
---
+++ static/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open('offline-cache').then(cache => {
+        return cache.addAll(['/static/offline.html']);
+    }));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match('/static/offline.html');
+        })
+    );
+});
