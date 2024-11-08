c9c7e461dfd9fbc8ed0d5e69287ddc93b442e47d
DeDxYk594 <zhugeo@mail.ru>
2024-11-08 16:36:56 +0300

    asdf

--- keys/gen_cert.sh
+++ keys/gen_cert.sh
@@ -10,8 +10,8 @@ mkdir -p $CERT_DIR
 ~/.acme.sh/acme.sh --issue -d 109.120.180.70 -d 109.120.180.70:8005 --webroot /var/www/html
 
 ~/.acme.sh/acme.sh --install-cert -d 109.120.180.70 -d 109.120.180.70:8005 \
-  --cert-file $CERT_DIR/fullchain.pem \
-  --key-file $CERT_DIR/privkey.pem
+  --cert-file ./fullchain.pem \
+  --key-file ./privkey.pem
 
 echo "Сертификаты на фронт и бэк успешно получены и установлены в $CERT_DIR."
 
