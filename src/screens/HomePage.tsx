ee1106b9f94cd474afafec3073125dedc655aec3
Konstantin Safronov <kostya.safronov@gmail.com>
2024-11-23 02:03:17 +0300

    landing update

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -8,6 +8,13 @@ import { goToUrl } from '@/stores/routerStore';
 
 import './homePage.scss';
 
+function scrollToSection(id: string): void {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' }); // Плавная прокрутка
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const HomePage = (props: ComponentProps) => {
   const [isRegistrationOpened, setIsRegistrationOpened] = useState(false);
@@ -61,17 +68,34 @@ export const HomePage = (props: ComponentProps) => {
               <img src="static/img/logo.svg" class="home-page__logo" />
             </div>
           </div>
+
+          <div className="homepage__scroll-indicator">
+            <a
+              href="#kanban-info"
+              className="scroll-link"
+              onclick={(e: MouseEvent) => {
+                e.preventDefault(); // Предотвращаем стандартное поведение ссылки
+                scrollToSection('kanban-info'); // Плавная прокрутка
+              }}
+            >
+              <div className="scroll-text">Подробнее о Pumpkin</div>
+              <i className="bi bi-arrow-down scroll-arrow"></i>
+            </a>
+          </div>
+
           <img src="/static/img/wave.svg" class="homepage__wave" />
         </div>
-        <div className="homepage__quote-section">
+
+        <div id="kanban-info" className="homepage__quote-section">
           <div className="homepage__quote">
             <strong>Канбан</strong> (яп. カンバン) - система организации задач,
             позволяющая придерживаться принципа "точно в срок"
           </div>
         </div>
+
         <div className="homepage__detail-section homepage__detail-section__odd">
           <div className="homepage__detail-image">
-            <img src="/static/img/detail1.png" />
+            <img src="/static/img/photo_desk.png" width="100%" />
           </div>
           <div className="homepage__detail-comment">
             <div className="homepage__detail-header">
@@ -83,10 +107,8 @@ export const HomePage = (props: ComponentProps) => {
             </div>
           </div>
         </div>
-        <div className="homepage__detail-section homepage__detail-section__even">
-          <div className="homepage__detail-image">
-            <img src="/static/img/detail2.png" />
-          </div>
+
+        <div className="homepage__detail-section homepage__detail-section__odd">
           <div className="homepage__detail-comment">
             <div className="homepage__detail-header">
               Размещайте задачи в колонках
@@ -96,10 +118,22 @@ export const HomePage = (props: ComponentProps) => {
               вы точно ничего не забудете
             </div>
           </div>
+          <div className="homepage__detail-image">
+            <img
+              src="/static/img/photo_columns.png"
+              alt="Колонки задач"
+              width="100%"
+            />
+          </div>
         </div>
+
         <div className="homepage__detail-section homepage__detail-section__odd">
           <div className="homepage__detail-image">
-            <img src="/static/img/detail3.png" />
+            <img
+              src="/static/img/photo_desk_settings.png"
+              alt="Работайте вместе"
+              width="100%"
+            />
           </div>
           <div className="homepage__detail-comment">
             <div className="homepage__detail-header">Работайте вместе</div>
@@ -109,6 +143,18 @@ export const HomePage = (props: ComponentProps) => {
             </div>
           </div>
         </div>
+
+        <div class="video-frame">
+          <iframe
+            src="https://drive.google.com/file/d/1zIoGZ8QWQVdl0K3-tuuay9XfbQEbu47i/preview"
+            width="854"
+            height="480"
+            frameborder="0"
+            allowfullscreen
+            title="Embedded Video"
+          ></iframe>
+        </div>
+
         <footer class="homepage__footer">
           (c) Team RPO, Texnopark Mail.ru, 2024
         </footer>
